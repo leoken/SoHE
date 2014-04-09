60a322d5c62d8deda9c36553eb4ad8a4c0134ca5
Erik James Albaugh <erik@maintainweb.co>
2014-04-09 05:17:24 -0700

    Minor Updates

--- lib/custom.php
+++ lib/custom.php
@@ -57,3 +57,20 @@ add_action( 'init', 'my_add_excerpts_to_pages' );
 function my_add_excerpts_to_pages() {
      add_post_type_support( 'page', 'excerpt' );
 }
+
+
+
+// http://wordpress.org/support/topic/blog-tab-gets-highlighted-in-nav-menu-for-custom-post-types
+function custom_fix_blog_tab_on_cpt($classes,$item,$args) {
+    if(!is_singular('post') && !is_category() && !is_tag()) {
+        $blog_page_id = intval(get_option('page_for_posts'));
+        if($blog_page_id != 0) {
+            if($item->object_id == $blog_page_id) {
+        unset($classes[array_search('active',$classes)]);
+      }
+        }
+    }
+    return $classes;
+}
+add_filter('nav_menu_css_class','custom_fix_blog_tab_on_cpt',10,3);
+
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,10 +1,3 @@
-<?php
-if ( is_singular('staff') || is_taxonomy('department') ) {?>
-<div class="page-header">
-  <h3>Who We Are</h3>
-  <h1>Faculty &amp; Staff<?php if ( is_archive('department') ) { ?> <span>/</span> <?php echo roots_title(); } ?></h1>
-</div>
-<?php } else { ?>
 <div class="page-header">
   <h3>
     <?php $parent_title = get_the_title($post->post_parent); ?>
@@ -14,4 +7,3 @@ if ( is_singular('staff') || is_taxonomy('department') ) {?>
     <?php echo roots_title(); ?>
   </h1>
 </div>
-<?php } ?>
