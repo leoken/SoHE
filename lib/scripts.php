76a4a7332ed18eb34bfc723ff8fc9e39ec9c913e
Erik James Albaugh <erik@maintainweb.co>
2014-04-28 15:05:05 -0700

    Sidebar Style Updates

--- lib/scripts.php
+++ lib/scripts.php
@@ -13,7 +13,7 @@
 function roots_scripts() {
   
   //wp_enqueue_style('app', get_template_directory_uri() . '/assets/css/app.css', false, '9880649384aea9f1ee166331c0a30daa');
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a9a6b811877949a77bc810ebf525a3d7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '547b699d99336fe88c61e08e81540064');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
